e78d5ac0099816d6f71360c732a1d1f10fe513bb
Shivamt765 <shivamt765@gmail.com>
2022-08-30 02:23:51 +0530

    Pokemon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
+import Axios from "axios";
 function App() {
+  const [pokemonName, setPokemonName] = useState("");
+  const [pokemonchosen, setPokemonChosen] = useState(false);
+  const [pokemon, setPokemon] = useState({
+    name: "",
+    species: "",
+    img: "",
+    hp: "",
+    attack: "",
+    defense: "",
+    type: "",
+  });
+  const searchPokemon = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then(
+      (response) => {
+        setPokemon({
+          name: pokemonName,
+          species: response.data.species.name,
+          img: response.data.sprites.front_default,
+          hp: response.data.stats[0].base_stats,
+          attack: response.data.stats[1].base_stats,
+          defense: response.data.stats[2].base_stats,
+          type: response.data.types[0].type.name,
+        });
+        setPokemonChosen(true);
+      }
+    );
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Title">
+        <h1>Pokemon Info</h1>
+        <input
+          type="text"
+          onChange={(event) => {
+            setPokemonName(event.target.value);
+          }}
+        />
+        <button onClick={searchPokemon}>Search</button>
+      </div>
+      <div className="DisplaySection">
+        {!pokemonchosen ? (
+          <h1>Please chose a pokemon</h1>
+        ) : (
+          <>
+            <h1>{pokemon.name}</h1>
+            <img src={pokemon.img}></img>
+            <h2>Species:{pokemon.species}</h2>
+            <h2>Type:{pokemon.type}</h2>
+            <h2>Hp:{pokemon.hp}</h2>
+            <h2>Attack:{pokemon.attack}</h2>
+            <h2>Defense:{pokemon.defense}</h2>
+          </>
+        )}
+      </div>
     </div>
   );
 }
